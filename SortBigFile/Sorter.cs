c0694c2a9c1f53cd6bc23c11867762ab0e09faa9
Sergey <ndhelix@gmail.com>
2017-12-21 15:53:44 +0300

    revert

--- SortBigFile/Sorter.cs
+++ SortBigFile/Sorter.cs
@@ -28,21 +28,21 @@ namespace SortBigFile
 
             // this block was used to test on small files
             _smallfilesize = _sortSizeLimit / 2;
-            if (_smallfilesize < size/500)
-                _smallfilesize = (int)(size/200);
+            if (_smallfilesize < size / 500)
+                _smallfilesize = (int)( size / 200 );
 
             List<string> smallfilelistsorted = CreateAndSortSmallFiles( filename );
-            File.AppendAllText( "sorter.log", "CreateSmallFiles took " + (DateTime.Now - start).ToString( @"hh\:mm\:ss" ) + Environment.NewLine );
+            File.AppendAllText( "sorter.log", "CreateSmallFiles took " + ( DateTime.Now - start ).ToString( @"hh\:mm\:ss" ) + Environment.NewLine );
             start = DateTime.Now;
 
             //List<string> smallfilelistsorted = SortSmallFiles( smallfilelist );
             //File.AppendAllText( "sorter.log", "SortSmallFiles took " + ( DateTime.Now - start ).ToString( @"hh\:mm\:ss" ) + Environment.NewLine );
             //start = DateTime.Now;
-            MergeFiles( smallfilelistsorted, sortedfilename+"merged", 0, true );
-            File.AppendAllText( "sorter.log", "MergeFiles took " + ( DateTime.Now - start ).ToString( @"hh\:mm\:ss" )+Environment.NewLine );
+            MergeFiles( smallfilelistsorted, sortedfilename + "merged", 0, true );
+            File.AppendAllText( "sorter.log", "MergeFiles took " + ( DateTime.Now - start ).ToString( @"hh\:mm\:ss" ) + Environment.NewLine );
         }
 
-        private void MergeFiles( List<string> filelist, string outputfilename, int recursionlevel, bool isroot = false)
+        private void MergeFiles( List<string> filelist, string outputfilename, int recursionlevel, bool isroot = false )
         {
             if (filelist.Count <= _mergepool)
             {
@@ -62,12 +62,12 @@ namespace SortBigFile
                 string mergedfile = filecounter.ToString( "0000" ) + "_merged_rec" + recursionlevel;
                 mergedfile = Path.Combine( dir, mergedfile );
                 mergedfilelist.Add( mergedfile );
-                Task task = Task.Run( () => MergeFiles( list, mergedfile, recursionlevel+2 ) );
+                Task task = Task.Run( () => MergeFiles( list, mergedfile, recursionlevel + 2 ) );
                 TaskList.Add( task );
                 filecounter++;
             }
             Task.WaitAll( TaskList.ToArray() );
-            MergeFiles( mergedfilelist, outputfilename, recursionlevel+1, isroot ) ;
+            MergeFiles( mergedfilelist, outputfilename, recursionlevel + 1, isroot );
         }
 
         private List<List<string>> GetSubLists( List<string> filelist, int maxcount )
@@ -77,13 +77,13 @@ namespace SortBigFile
                 listlist.Add( filelist.GetRange( i, Math.Min( maxcount, filelist.Count - i ) ) );
             return listlist;
         }
-        private  void MergePlain( List<string> filelist, string outputfilename, bool unswap )
+        private void MergePlain( List<string> filelist, string outputfilename, bool unswap )
         {
             int buffersize = 50000;
             StreamWriter writer = new StreamWriter( File.Open( outputfilename, FileMode.Create ) );
             StreamReader[] srarr = new StreamReader[filelist.Count];
             Queue<string>[] q = new Queue<string>[filelist.Count];
-            
+
             for (int i = 0; i < filelist.Count; i++)
                 q[i] = new Queue<string>();
 
@@ -104,60 +104,60 @@ namespace SortBigFile
             var sd = new SortedDictionary<string, int>();
             for (int i = 0; i < q.Length; i++)
                 if (q[i].Count > 0)
-                    sd.AddAnyway( q[i].Dequeue(), i);
+                    sd.AddAnyway( q[i].Dequeue(), i );
 
             if (sd.Any())
-            while (true)
-            {
-                if (!sd.Any())
+                while (true)
                 {
-                    for (int i = 0; i < filelist.Count; i++)
+                    if (!sd.Any())
+                    {
+                        for (int i = 0; i < filelist.Count; i++)
+                        {
+                            int qlength = 0;
+                            if (!q[i].Any())
+                                while (!srarr[i].EndOfStream && qlength < buffersize)
+                                {
+                                    string line = srarr[i].ReadLine();
+                                    q[i].Enqueue( line );
+                                    qlength++;
+                                }
+                        }
+
+                        for (int i = 0; i < q.Length; i++)
+                            if (q[i].Count > 0)
+                                sd.AddAnyway( q[i].Dequeue(), i );
+                    }
+
+                    if (!sd.Any())
+                        break;
+
+                    int filenum = sd.First().Value;
+                    string row = sd.First().Key.TrimEnd( '\t' );
+                    writer.WriteLine( unswap ? UnSwap( row ) : row );
+                    sd.Remove( sd.First().Key );
+                    if (!q[filenum].Any())
                     {
                         int qlength = 0;
-                        if (!q[i].Any())
-                            while (!srarr[i].EndOfStream && qlength < buffersize)
-                            {
-                                string line = srarr[i].ReadLine();
-                                q[i].Enqueue( line );
-                                qlength++;
-                            }
+                        while (!srarr[filenum].EndOfStream && qlength < buffersize)
+                        {
+                            string line = srarr[filenum].ReadLine();
+                            q[filenum].Enqueue( line );
+                            qlength++;
+                        }
                     }
 
-                    for (int i = 0; i < q.Length; i++)
-                        if (q[i].Count > 0)
-                            sd.AddAnyway( q[i].Dequeue(), i);
-                }
-
-                if (!sd.Any())
-                    break;
-
-                int filenum = sd.First().Value;
-                string row = sd.First().Key.TrimEnd('\t');
-                writer.WriteLine( unswap ? UnSwap( row ) : row );
-                sd.Remove( sd.First().Key );
-                if (!q[filenum].Any())
-                {
-                    int qlength = 0;
-                    while (!srarr[filenum].EndOfStream && qlength < buffersize)
+                    if (q[filenum].Any())
                     {
-                        string line = srarr[filenum].ReadLine();
-                        q[filenum].Enqueue( line );
-                        qlength++;
+                        string key = q[filenum].Dequeue();
+                        sd.AddAnyway( key, filenum );
                     }
-                }
 
-                if (q[filenum].Any())
-                {
-                    string key = q[filenum].Dequeue();
-                    sd.AddAnyway( key, filenum );
                 }
-
-            }
 
             for (int i = 0; i < filelist.Count; i++)
                 srarr[i].Dispose();
 
-            foreach(string filename in filelist)
+            foreach (string filename in filelist)
                 if (File.Exists( filename ))
                     File.Delete( filename );
 
@@ -194,62 +194,47 @@ namespace SortBigFile
             List<string> files = new List<string>();
             //StreamWriter sw;
             var TaskList = new List<Task>();
-            Queue<string> q = new Queue<string>();
+            int filecounter = 0;
 
             using (StreamReader sr = new StreamReader( filename ))
             {
                 var list = new List<string>( 10000000 );
                 //int counter = 0;
-                int filecounter = 0;
                 long currentsize = 0;
                 string line;
                 string smallfile = "";
                 //sw = new StreamWriter( File.Open( smallfile, FileMode.Create ) );
-                while ((line = sr.ReadLine()) != null)
+                
+                while (( line = sr.ReadLine() ) != null)
                 {
                     if (currentsize >= _smallfilesize)
                     {
                         smallfile = Path.Combine( sortareadir, filecounter.ToString( "0000" ) );
-                        q.Enqueue( smallfile );
+
                         WriteListToFile( list, smallfile );
 
-                        if (q.Count >= 8)
-                            while(q.Any())
-                            {
-                                string file = q.Dequeue();
-                                string sortedfile = file + "_sorted";
-                                Task task = Task.Run( () => SortSmallFile( file, sortedfile, false ) );
-                                TaskList.Add( task );
-                                files.Add( sortedfile );    
-                            }
+                        string sortedfile = smallfile + "_sorted";
+                        Task task = Task.Run( () => SortSmallFile( smallfile, sortedfile, false ) );
+                        TaskList.Add( task );
+                        files.Add( sortedfile );
 
                         filecounter++;
                         list.Clear();
                         currentsize = 0;
                     }
-                    list.Add( Swap(line) );
-                    currentsize+=line.Length;
+                    list.Add( Swap( line ) );
+                    currentsize += line.Length;
                 }
                 if (list.Count > 0)
                 {
                     smallfile = Path.Combine( sortareadir, filecounter.ToString( "0000" ) );
                     WriteListToFile( list, smallfile );
-                    q.Enqueue( smallfile );
-                    //string sortedfile = smallfile + "_sorted";
-                    //Task task = Task.Run( () => SortSmallFile( smallfile, sortedfile, false ) );
-                    //TaskList.Add( task );
-                    //files.Add( sortedfile );
+                    string sortedfile = smallfile + "_sorted";
+                    Task task = Task.Run( () => SortSmallFile( smallfile, sortedfile, false ) );
+                    TaskList.Add( task );
+                    files.Add( sortedfile );
                 }
                 list = null;
-            }
-
-            while (q.Any())
-            {
-                string file = q.Dequeue() ;
-                string sortedfile = file + "_sorted";
-                Task task = Task.Run( () => SortSmallFile( file, sortedfile, false ) );
-                TaskList.Add( task );
-                files.Add( sortedfile );
             }
 
             Task.WaitAll( TaskList.ToArray() );
@@ -278,7 +263,7 @@ namespace SortBigFile
             {
                 int i = 0;
                 string line;
-                while ((line = sr.ReadLine()) != null)
+                while (( line = sr.ReadLine() ) != null)
                 {
                     if (swap)
                         line = Swap( line );
@@ -292,7 +277,7 @@ namespace SortBigFile
             {
                 foreach (string item in list)
                 {
-                    bw.WriteLine( swap ? UnSwap( item ): item );
+                    bw.WriteLine( swap ? UnSwap( item ) : item );
                 }
             }
             if (File.Exists( filename ))
